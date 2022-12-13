219e6b3013796d060bcb4ec8280ee23b5ff08b5a
AramAroian <aramaroian1@gmail.com>
2022-12-13 20:56:59 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   assets/styles.css
    
            added class components to the rendered cards
            should revise down the road
    
    	modified:   index.html
    
            removed dummy HTML to inject via js
    
    	modified:   index.js
    
            switched storing method to localstorage
            added general render method to use for each link in the navbar
            added rendering method for homepage

--- index.js
+++ index.js
@@ -1,20 +1,71 @@
 $(function () {
+  //event listeners
+
+  let coins = [];
+  const coinDiv = $("#allCoinDisplay");
+
   // Getting info from API
 
   function getCoinData() {
     $.ajax({
       url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
-      success: (data) => console.log(data),
+      success: (data) => localStorage.setItem("coins", JSON.stringify(data)),
       error: (err) => console.error(err.status, err.responseText),
     });
   }
 
-  function storeData(data) {}
+  // Storing coins to localstorage
 
-  getCoinData();
+  function storeCoins() {
+    coins = JSON.parse(localStorage.getItem("coins"));
+  }
+
+  // Render homepage
+
+  function renderHomePage() {
+    $.when(getCoinData()).done(() => {
+      storeCoins();
+      console.log(coins);
+      coins.forEach(function (coin) {
+        coinDiv.append(`
+        <div id="allCoinDisplay" class="card-group">
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+          <img src="${coin.image}" class="card-custom-img" alt="...">
+            <h5 class="card-title">${coin.symbol}</h5>
+            <p class="card-text">${coin.name}</p>
+            <a href="#" class="btn btn-primary">More info</a>
+          </div>
+          <div class="container">
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span>
+            </label>
+          </div>
+        </div>
+      </div>
+
+      `);
+      });
+    });
+  }
+
+  // function renderData() {
+  //   coins = storeCoins();
+
+  // coinDiv.append(``);
+  // }
+
+  // templete for rendering
+
+  function renderAnything() {
+    $.when(getCoinData()).done(() => {
+      storeCoins();
+      console.log(coins);
+    });
+  }
+
+  //testing ground
+  // getCoinData();
+  renderHomePage();
 });
-
-//100 first coins by market cap:
-// curl -X 'GET' \'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false' \  -H 'accept: application/json'
-
-// req url: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
