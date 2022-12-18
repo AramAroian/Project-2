993bbecbf6a79a11949823e13da2f0296b4e6f2c
AramAroian <aramaroian1@gmail.com>
2022-12-18 23:36:30 +0200

    new file:   assets/background.jpg         added new background
    	modified:   index.html
            added:
            - paralax background
    
            changed:
            - coinDiv to mainDiv
    	modified:   index.js
            added:
            - handleSelection()
            - addCoin()
            - removeCoin()
            - searchCoins()
            - event listeners for navbar elements
            - rendering home on load

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 $(function () {
-  //event listeners
-
+  //event listeners and variables
   let coins = [];
-  const coinDiv = $("#allCoinDisplay");
+  let selectedCoins = [];
+
+  const home = $("#home");
+  home.on("click", renderHomePage);
+
+  const reports = $("#reports");
+  reports.on("click");
+
+  const about = $("#about");
+  about.on("click");
+
+  const mainDiv = $("#mainDiv");
+  mainDiv.on("change", "input", handleSelection);
+
+  const searchInput = $("#search");
+  searchInput.keyup(function () {
+    searchCoins($(this).val());
+  });
+
+  // const searchButton = $("#searchButton");
+  // searchButton.on("click", searchCoin);
 
   // Getting info from API
 
@@ -25,36 +44,71 @@ $(function () {
   function renderHomePage() {
     $.when(getCoinData()).done(() => {
       storeCoins();
-      console.log(coins);
       coins.forEach(function (coin) {
-        coinDiv.append(`
-        <div id="allCoinDisplay" class="card-group">
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-          <img src="${coin.image}" class="card-custom-img" alt="...">
-            <h5 class="card-title">${coin.symbol}</h5>
-            <p class="card-text">${coin.name}</p>
-            <a href="#" class="btn btn-primary">More info</a>
-          </div>
+        let newCoin = `
+        <div>
+        <div class="row row-cols-1 row-cols-md-4" >
+          <div class="card" style="width: 18rem">
+            <div class="card-body">
+              <img src="${coin.image}" class="card-custom-img" alt="...">
+              <h5 class="card-title">${coin.symbol}</h5>
+              <p class="card-text">${coin.name}</p>
+              <a href="#" class="btn btn-primary">More info</a>
+            </div>
           <div class="container">
             <label class="switch">
-              <input type="checkbox" />
+              <input type="checkbox" id="${coin.symbol}" />
               <span class="slider round"></span>
             </label>
           </div>
         </div>
       </div>
 
-      `);
+      `;
+        mainDiv.append(newCoin);
       });
     });
   }
 
-  // function renderData() {
-  //   coins = storeCoins();
+  // navigation functions
 
-  // coinDiv.append(``);
-  // }
+  // selects a specific coin
+
+  function handleSelection(event) {
+    if (event.target.checked) {
+      addCoin(event);
+    } else {
+      removeCoin(event);
+    }
+  }
+
+  function addCoin(event) {
+    id = event.target.id;
+    let result = coins.find((coin) => coin.symbol == id);
+    if (selectedCoins.length < 5) {
+      selectedCoins.push(result);
+      console.log(selectedCoins);
+    } else {
+      $(`#${id}`).prop("checked", false);
+      console.log("selected to many you greedy bastard");
+    }
+  }
+
+  function removeCoin(event) {
+    id = event.target.id;
+    let result = coins.find((coin) => coin.symbol == id);
+    selectedCoins = $.grep(selectedCoins, (value) => {
+      return value != result;
+    });
+    console.log(selectedCoins);
+  }
+
+  // function for searching for coins
+
+  function searchCoins(searchText) {
+    // let result = Array.from(coins, (coin) => coin.symbol);
+    console.log(searchText);
+  }
 
   // templete for rendering
 
@@ -66,6 +120,5 @@ $(function () {
   }
 
   //testing ground
-  // getCoinData();
-  renderHomePage();
+  $(document).ready(renderHomePage());
 });
