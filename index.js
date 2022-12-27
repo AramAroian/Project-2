b975b0afc066610a740ad72b4667e17eee329d38
AramAroian <aramaroian1@gmail.com>
2022-12-27 20:40:53 +0200

    modified:   assets/styles.css         - added collapse margin 	modified:   index.html 	modified:   index.js         - changed selectedCoins [] to followedCoins []         - saved followed coins to localStorage         - modal save changes features fully functional         - modal cancel requires development         - added more info collapse to rendered cards.         - added more info AJAX queries and saved to localStorage.         - started rendering price info to collapse cards
        Things to do:
        - Set 2 min timeout to more info AJAX calls if info already is in localStorage.
        - Sort null on first call in renderInfo
        - develop modal cancel.
        - develop search functionality.
        - develop about page.
        - develop live reports page.
        - make sure site compatible for smartphone res

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 $(function () {
   //event listeners and variables
   let coins = [];
-  let selectedCoins = [];
+  let coinPrice = [];
+  let followedCoins = [];
+  let tempSelected;
+
+  let sessionData = [];
+
+  let searchArr = [];
 
   const home = $("#home");
   home.on("click", renderHomePage);
@@ -14,14 +20,22 @@ $(function () {
 
   const mainDiv = $("#mainDiv");
   mainDiv.on("change", "input", handleSelection);
+  mainDiv.on("click", "a", renderInfo);
 
-  const searchInput = $("#search");
-  searchInput.keyup(function () {
-    searchCoins($(this).val());
-  });
+  const coinModal = $("#staticBackdrop");
+  const coinModalBody = $(".table-body");
+  coinModalBody.on("change", "input", handleSelection);
+
+  const modalSaveBtn = $("#saveChanges");
+  modalSaveBtn.on("click", handleSave);
+
+  // const searchInput = $("#search");
+  // searchInput.keyup(function () {
+  //   searchCoins($(this).val().toLowerCase());
+  // });
 
   // const searchButton = $("#searchButton");
-  // searchButton.on("click", searchCoin);
+  // searchButton.on("click", searchCoins(searchInput.val()));
 
   // Getting info from API
 
@@ -33,44 +47,128 @@ $(function () {
     });
   }
 
-  // Storing coins to localstorage
+  function getPriceData(id) {
+    $.ajax({
+      url: `https://api.coingecko.com/api/v3/coins/${id}`,
+      success: (data) => localStorage.setItem(`${id}`, JSON.stringify(data)),
+      error: (err) => console.error(err.status, err.responseText),
+    });
+  }
 
-  function storeCoins() {
+  // $('#loadingDiv')
+  // .hide()
+  // .ajaxStart(function() {
+  //     $(this).show();
+  // })
+  // .ajaxStop(function() {
+  //     $(this).hide();
+  // });
+
+  // getting data from localstorage
+
+  function getStoredCoins() {
     coins = JSON.parse(localStorage.getItem("coins"));
+  }
+
+  function getStoredPrices(id) {
+    return JSON.parse(localStorage.getItem(`${id}`));
   }
 
   // Render homepage
 
   function renderHomePage() {
     $.when(getCoinData()).done(() => {
-      storeCoins();
-      coins.forEach(function (coin) {
-        let newCoin = `
-        <div>
+      getStoredCoins();
+      renderMainDiv(coins, mainDiv);
+    });
+  }
+
+  function renderMainDiv(arr, mainDiv) {
+    mainDiv.html("");
+    let newCoin;
+    let isChecked;
+    getFollowedCoins();
+
+    arr.forEach(function (coin) {
+      if (
+        followedCoins !== null &&
+        followedCoins.some((item) => item.symbol === coin.symbol)
+      ) {
+        isChecked = 'checked="true"';
+      } else {
+        isChecked = "";
+      }
+
+      newCoin = `
+  
         <div class="row row-cols-1 row-cols-md-4" >
-          <div class="card" style="width: 18rem">
+          <div  class="card coin-card">
             <div class="card-body">
               <img src="${coin.image}" class="card-custom-img" alt="...">
               <h5 class="card-title">${coin.symbol}</h5>
               <p class="card-text">${coin.name}</p>
-              <a href="#" class="btn btn-primary">More info</a>
+              
+            <p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" id="${coin.id}" href="#moreInfo_${coin.id}" role="button" aria-expanded="false" aria-controls="moreInfo_${coin.id}">
+              More info
+            </a>
+          </p>
+             
             </div>
+
+          <div class="collapse" id="moreInfo_${coin.id}">
+            <div class="card card-body ${coin.id}" id="">
+
+              
+            </div>
+          </div>
+
           <div class="container">
             <label class="switch">
-              <input type="checkbox" id="${coin.symbol}" />
+              <input type="checkbox" class="checkbox" id="${coin.symbol}" ${isChecked}  />
               <span class="slider round"></span>
             </label>
           </div>
         </div>
-      </div>
-
+        </div>
+  
+  
       `;
-        mainDiv.append(newCoin);
-      });
+      mainDiv.append(newCoin);
     });
   }
 
   // navigation functions
+
+  // handling more info
+
+  function renderInfo(event) {
+    let id = event.target.id;
+    let coinInfo;
+    if (localStorage.getItem(`${id}`) === null) {
+      $.when(getPriceData(id)).then(() => {
+        coinInfo = getStoredPrices(id);
+        console.log("new data fetched");
+      });
+    } else {
+      coinInfo = getStoredPrices(id);
+      console.log("data already exists");
+    }
+
+    $(`.${id}`).html("");
+    $(`.${id}`).append(
+      `Current coin price is: <br/> ${coinInfo.market_data.current_price.usd}$ <br/> ${coinInfo.market_data.current_price.eur}€ <br/> ${coinInfo.market_data.current_price.ils}₪`
+    );
+  }
+
+  //store selected coins
+  function setFollowedCoins(arr) {
+    localStorage.setItem("followedCoins", JSON.stringify(arr));
+  }
+
+  function getFollowedCoins() {
+    followedCoins = JSON.parse(localStorage.getItem("followedCoins"));
+  }
 
   // selects a specific coin
 
@@ -85,39 +183,81 @@ $(function () {
   function addCoin(event) {
     id = event.target.id;
     let result = coins.find((coin) => coin.symbol == id);
-    if (selectedCoins.length < 5) {
-      selectedCoins.push(result);
-      console.log(selectedCoins);
+    getFollowedCoins();
+    if (followedCoins == null) {
+      let arr = [];
+      arr.push(result);
+      followdCoins = setFollowedCoins(arr);
+    } else if (followedCoins.length < 5) {
+      followedCoins.push(result);
+      setFollowedCoins(followedCoins);
     } else {
-      $(`#${id}`).prop("checked", false);
-      console.log("selected to many you greedy bastard");
+      tempSelected = result;
+      setTimeout(() => {
+        $(`#${id}`).prop("checked", false);
+      }, 300);
+      renderModal(followedCoins);
+      coinModal.modal("show");
     }
   }
 
   function removeCoin(event) {
     id = event.target.id;
-    let result = coins.find((coin) => coin.symbol == id);
-    selectedCoins = $.grep(selectedCoins, (value) => {
-      return value != result;
+    let index = followedCoins.findIndex((coin) => coin.symbol == id);
+    followedCoins.splice(index, 1);
+    setFollowedCoins(followedCoins);
+  }
+
+  function updateSavedCoins() {
+    if (followedCoins.length < 5) {
+      followedCoins.push(tempSelected);
+      setFollowedCoins(followedCoins);
+    }
+  }
+
+  function handleSave() {
+    updateSavedCoins();
+    renderHomePage();
+  }
+
+  function renderModal(arr) {
+    coinModalBody.html("");
+    arr.forEach(function (coin) {
+      let newCoin = `
+
+    <tr>
+      <th scope="row"><img src="${coin.image}" class="card-custom-img" alt="${this.name}"></th>
+      <td class="title">${coin.symbol}</td>
+      <td>
+      <label class="switch">
+      <input type="checkbox" id="${coin.symbol}" checked="true"  />
+      <span class="slider round"></span>
+      </label>
+      </td>
+    </tr>
+
+    `;
+
+      coinModalBody.append(newCoin);
     });
-    console.log(selectedCoins);
   }
 
-  // function for searching for coins
+  // function for searching for coins on type
 
-  function searchCoins(searchText) {
-    // let result = Array.from(coins, (coin) => coin.symbol);
-    console.log(searchText);
-  }
+  // function searchCoins(searchText) {
+  //   searchArr = [];
 
-  // templete for rendering
+  //   $(`.coin-card:contains(${searchText})`).each(function () {
+  //     searchArr.push($(this));
+  //   });
 
-  function renderAnything() {
-    $.when(getCoinData()).done(() => {
-      storeCoins();
-      console.log(coins);
-    });
-  }
+  //   if (searchArr.length != 0) {
+  //     renderMainDiv(searchArr, mainDiv);
+  //   } else {
+  //     renderHomePage();
+  //   }
+  //   console.log(searchArr);
+  // }
 
   //testing ground
   $(document).ready(renderHomePage());
